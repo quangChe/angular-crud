3a996f2f8cd36ad1cadda083be4e0ccd9ef86892
Quang-Che <quang.n.che@gmail.com>
2017-11-18 11:34:08 -0800

    Feat: Minor stylistic changes and improvements
    This commit provides minor changes to the styles to improve the
    appearance of the application. Also, the script tags were moved into
    the head to make sure AngularJS is loaded before the elements are
    loaded and display empty data bindings.

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-let app = angular.module('crudApp', []);
+const app = angular.module('crudApp', []);
+
 
 app.controller('appCtrl', ['$scope', ($scope) => {
   $scope.currentView = 'listView';
-  $scope.friends = [
-    {
-      name: "Quang Che",
-      address: "El Monte, CA",
-      age: 25,
-      occupation: "Junior Web Developer",
-      image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Brad_Pitt_Fury_2014.jpg"
-    },
-    {
-      name: "Thor",
-      address: "Castle, Asgard",
-      age: 5033,
-      occupation: "God of Thunder/Guardian of Midgard",
-      image: "https://heroichollywood.b-cdn.net/wp-content/uploads/2017/08/Thor_Ragnarok_Chris_Hemsworth.jpg?x42694"
-    },
-    {
-      name: "Jennifer Lawrence",
-      address: "Hollywood, CA",
-      age: 27,
-      occupation: "Bubbly Actress",
-      image: "https://media.vanityfair.com/photos/5432e75e74ed87dd423a383a/master/h_590,c_limit/jennifer-lawrence-vanity-fair-11-november-cover-ss03.jpg"
-    },
-    {
-      name: "Michael Scott",
-      address: "1725 Slough Avenue in Scranton, PA",
-      age: 53,
-      occupation: "Manager, Leader, Captain, Friend",
-      image: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest/scale-to-width-down/350?cb=20170701090332"
-    }
-];
   $scope.editIndex = '';
   $scope.deleteIndex = '';
+  $scope.friends = data;
 
   $scope.changeView = (val) => {
     $scope.currentView = val;
